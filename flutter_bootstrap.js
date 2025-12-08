56f02e49d730cad784e7f52601ad93e15193d608
ZaferGunal <122797651+ZaferGunal@users.noreply.github.com>
2025-12-08 10:25:28 +0300

    j
    k

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"b9afb03275c7cf04906cb5b94faa60005561fc
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "443687166" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2844977937" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
